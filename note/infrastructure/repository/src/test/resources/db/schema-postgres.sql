ce90194e20afa6f5a9c87abce00760f226400b23
mingshashan <mingshashan@gmail.com>
2022-04-08 15:28:05 +0800

    update ddl sql

--- note/infrastructure/repository/src/test/resources/db/schema-postgres.sql
+++ note/infrastructure/repository/src/test/resources/db/schema-postgres.sql
@@ -1,4 +1,4 @@
-create table note_article
+create table IF NOT EXISTS "take-note".note_article
 (
     id                VARCHAR(64)  NOT NULL,
     name              VARCHAR(128) NOT NULL,
@@ -11,22 +11,22 @@ create table note_article
     gmt_modified      timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     creator           varchar(32)           DEFAULT NULL,
     modifier          varchar(32)           DEFAULT NULL,
-    is_valid          tinyint(4) DEFAULT NULL COMMENT,
+    is_valid          smallint DEFAULT NULL,
     PRIMARY KEY (id)
 );
 CREATE
-INDEX note_article_name ON note_article (name);
-comment on table note_article is '文章表';
+INDEX note_article_name ON "take-note".note_article (name);
+comment on table "take-note".note_article is '文章表';
 
-comment on column note_article.id                   is 'ID';
-comment on column note_article.name                 is '文章名';
-comment on column note_article.title                is '文章标题';
-comment on column note_article.back_ground_image    is '文章标题';
-comment on column note_article.content              is '文章内容';
-comment on column note_article.category             is '文章所属目录';
-comment on column note_article.tags                 is '文章标签';
-comment on column note_article.gmt_create           is '创建时间';
-comment on column note_article.gmt_modified         is '修改时间';
-comment on column note_article.creator              is '创建人';
-comment on column note_article.modifier             is '修改人';
-comment on column note_article.is_valid             is '有效标识';
+comment on column "take-note".note_article.id                   is 'ID';
+comment on column "take-note".note_article.name                 is '文章名';
+comment on column "take-note".note_article.title                is '文章标题';
+comment on column "take-note".note_article.back_ground_image    is '文章标题';
+comment on column "take-note".note_article.content              is '文章内容';
+comment on column "take-note".note_article.category             is '文章所属目录';
+comment on column "take-note".note_article.tags                 is '文章标签';
+comment on column "take-note".note_article.gmt_create           is '创建时间';
+comment on column "take-note".note_article.gmt_modified         is '修改时间';
+comment on column "take-note".note_article.creator              is '创建人';
+comment on column "take-note".note_article.modifier             is '修改人';
+comment on column "take-note".note_article.is_valid             is '有效标识';
